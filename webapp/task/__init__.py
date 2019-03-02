06e351eeadeacf245c3b9158660ab44b26e87ac9
wangshumin <wangshumin@dianwoba.com>
2019-03-02 23:54:46 +0800

    delete celery_helper

--- webapp/task/__init__.py
+++ webapp/task/__init__.py
@@ -10,8 +10,12 @@ def make_celery(app):
     #     broker=app.config['CELERY_BROKER_URL']
     # )
     celery.main = app.import_name
-    celery.__autoset('result_backend', app.config['CELERY_RESULT_BACKEND'])
-    celery.__autoset('broker_url', app.config['CELERY_BROKER_URL'])
+    # celery.__autoset('result_backend', app.config['CELERY_RESULT_BACKEND'])
+    # celery.__autoset('broker_url', app.config['CELERY_BROKER_URL'])
+
+    celery.backend = app.config['CELERY_RESULT_BACKEND']
+    celery.broker_connection = app.config['CELERY_BROKER_URL']
+
     celery.conf.update(app.config)
 
     class ContextTask(celery.Task):
