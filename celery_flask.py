9904e985097c599f8513edc5113ff881c48895c1
wangshumin <wangshumin@dianwoba.com>
2019-03-14 19:35:18 +0800

    ++

--- celery_flask.py
+++ celery_flask.py
@@ -6,4 +6,4 @@ flask_app, socketio = create_app()
 
 if __name__ == '__main__':
     # flask_app.run(host='0.0.0.0', port=8888, debug=True)
-    socketio.run(flask_app, host='0.0.0.0', port=8888, debug=True)
+    socketio.run(flask_app, host='0.0.0.0', port=9999, debug=True)
