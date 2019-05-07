0aa8fd7ac34cd64a4c208a4ca066f437475d4c01
wangshumin <wangshumin@dianwoba.com>
2019-05-07 17:45:00 +0800

    test

--- webapp/task/__init__.py
+++ webapp/task/__init__.py
@@ -15,10 +15,10 @@ def make_celery(app):
     # )
     celery.main = app.import_name
     celery.conf.broker_url = app.config['CELERY_BROKER_URL']
-    celery.conf.result_backend = app.config['CELERY_BROKER_URL']
+    # celery.conf.result_backend = app.config['CELERY_BROKER_URL']
     if app.config.get('CELERY_BROKER_TRANSPORT_OPTIONS'):
         celery.conf.broker_transport_options = app.config['CELERY_BROKER_TRANSPORT_OPTIONS']
-        celery.conf.result_backend_transport_options = app.config['CELERY_BROKER_TRANSPORT_OPTIONS']
+        # celery.conf.result_backend_transport_options = app.config['CELERY_BROKER_TRANSPORT_OPTIONS']
     celery.conf.update(app.config)
 
     class ContextTask(celery.Task):
