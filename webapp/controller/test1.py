9dc620e7e4061d5ea5e3cdb988d0f5fefdf9951e
wangshumin <wangshumin@dianwoba.com>
2019-05-07 19:32:01 +0800

    test

--- webapp/controller/test1.py
+++ webapp/controller/test1.py
@@ -1,6 +1,6 @@
 # -*- coding=utf-8 -*-
 from . import controller
-from webapp.task.tasks import exec_task
+from webapp.task.tasks import exec_task, exec_task2, kill_celery_task
 from flask import request
 
 
@@ -14,4 +14,20 @@ def task():
     a = request.args.get("a", default=12)
     b = request.args.get("b", default=12)
     exec_task.delay(int(a), int(b))
+    return "task"
+
+
+@controller.route('/task2', methods=['GET', 'POST'])
+def task2():
+    a = request.args.get("a", default=12)
+    b = request.args.get("b", default=12)
+    name = request.args.get("name", default="aaa")
+    exec_task2.apply_async(args=(a, b), task_id=name)
+    return "task"
+
+
+@controller.route('/kill_task', methods=['GET', 'POST'])
+def kill_task():
+    name = request.args.get("name", default="aaa")
+    kill_celery_task.apply_async(args=(name, ), priority=0)
     return "task"
--- webapp/task/tasks.py
+++ webapp/task/tasks.py
@@ -1,7 +1,22 @@
 # -*- coding=utf-8 -*-
+from time import sleep
+
 from . import celery
 
 
 @celery.task()
 def exec_task(n1, n2):
     return n1 + n2
+
+
+@celery.task()
+def exec_task2(n1, n2):
+    for i in range(100):
+        print(i)
+        sleep(5)
+    return n1 + n2
+
+
+@celery.task()
+def kill_celery_task(task_id):
+    celery.control.terminate(task_id)
