14440df17dbe6486d4989ab4d7a5ac49525d5335
wangshumin <wangshumin@dianwoba.com>
2019-05-17 10:36:41 +0800

    test

--- webapp/task/__init__.py
+++ webapp/task/__init__.py
@@ -24,7 +24,7 @@ def make_celery(app):
     celery.conf.beat_schedule = {
         'save_user_info': {
             'task': 'webapp.task.bbeat.bbbb_task',
-            'schedule': crontab(hour=10, minute=35),
+            'schedule': crontab(hour=10, minute=40),
             'args': (123,)
         }
     }
